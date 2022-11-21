4dc019c79f2a8f14bde2fb91f81bc4dd4181575d
Yogesh Kumar <yk6130814@gmail.com>
2022-11-21 18:37:11 +0530

    Add project files.

---
+++ BuklyBook.DataAccess/Migrations/20221119121826_AddCategoryToDatabase.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BulkyBook.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCategoryToDatabase : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
